f5531ff634d87ac08f30fff1e85782dc6517b67c
Charlie Bukow <mik_25@outlook.com>
2025-12-17 12:19:05 +0000

    Add files via upload

---
+++ src/const.js
@@ -0,0 +1,46 @@
+// Error messages
+export const ERROR = {
+  EXPIRED_API_KEY: 'Expired API key',
+  ACCESS_DENIED: 'Access denied',
+  TWO_FACTOR_AUTH: 'Two factor authentication required',
+  NOT_PREMIUM: 'Premium account required',
+  NOT_READY: 'Torrent not ready',
+  INVALID_API_KEY: 'Invalid API key',
+};
+
+// Supported video extensions
+export const VIDEO_EXTENSIONS = [
+  '.mp4', '.mkv', '.avi',
+  '.mov', '.wmv', '.flv',
+  '.webm', '.m4v', '.ts',
+];
+
+// Stream sources
+export const STREAM_SOURCES = {
+  cf_jacred: {
+    url : 'https://extto.h-y.workers.dev',
+    name: 'ᒍᗩᑕᖇEᗪ'
+  },
+  cf_btsow: {
+    url : 'https://chrome-en.h-y.workers.dev',
+    name: '𝐁𝐓𝐒𝐨𝐰'
+  },
+  cf_testittv: {
+    url : 'https://testittv.h-y.workers.dev',
+    name: '𝙻𝙸𝙼𝙴𝚃𝙾𝚁𝚁𝙴𝙽𝚃𝚂'
+  },
+  cf_1337: {
+    url : 'https://1337x.h-y.workers.dev',
+    name: '1️3️⃣3️⃣7️⃣'
+    },
+  cf_yts: {
+    url : 'https://vercel-tgx.vercel.app',
+    name: 'TOᖇᖇEᑎTGᗩᒪᗩ᙭Y'
+  },
+  cf_uindex: {
+    url: 'https://uindex.h-y.workers.dev',
+    name: '【ＵＩＮＤＥＸ】'
+  },
+};
+
+};
